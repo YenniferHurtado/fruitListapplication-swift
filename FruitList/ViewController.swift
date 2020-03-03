6ce454b950520c9a18c793bcd3217982ab7b0856
YferHurtado <yennifer.hurtadoarce@gmail.com>
2020-03-03 16:50:01 -0500

    Initial Commit

---
+++ FruitList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FruitList
+//
+//  Created by Macbook Air 13 on 3/3/20.
+//  Copyright © 2020 Macbook Air 13. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
