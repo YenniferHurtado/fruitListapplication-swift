c86ab8ecea125fb16f67bdf6902728297ed69a14
YferHurtado <yennifer.hurtadoarce@gmail.com>
2020-03-04 22:43:28 -0500

    FruitList App

--- FruitList/AppDelegate.swift
+++ FruitList/AppDelegate.swift
@@ -1,17 +1,8 @@
-//
-//  AppDelegate.swift
-//  FruitList
-//
-//  Created by Macbook Air 13 on 3/3/20.
-//  Copyright © 2020 Macbook Air 13. All rights reserved.
-//
 
 import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -31,7 +22,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
 }
 
